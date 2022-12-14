a7e09c7e7ee0901d0839341e48d14464b1ea46a0
Vernar <enecelis2021@elam.co.uk>
2022-12-14 14:42:44 +0000

    continuation

---
+++ AI machine learning/Assets/Scripts/CarController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Vector3 startPosition, startRotation;
+
+    [Range(-1, 1f)]
+    public float a, t;
+
+    public float timeSinceStart = 0f;
+
+    [Header("Fitness")]
+    public float overallFitness;
+    public float distanceMultiplier = 1.4f;
+    public float avgSpeedMultiplier = 0.2f;
+
+    private Vector3 lastPosition;
+    private float totalDistanceTravelled;
+    private float avgSpeed;
+
+    private float aSensor, bSensor, cSensor;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        startRotation = transform.eulerAngles;
+
+    }
+
+
+    public void Reset()
+    {
+        timeSinceStart = 0f;
+        totalDistanceTravelled = 0f;
+        avgSpeed = 0f;
+        lastPosition = startPosition;
+        overallFitness = 0f;
+        transform.position = startPosition;
+        transform.eulerAngles = startRotation;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Reset();
+    }
+
+    private Vector3 inp;
+    public void MoveCar (float v, float h)
+    {
+        inp = Vector3.Lerp(Vector3.zero, new Vector3(0, 0, v * 11.4f), 0.02f);
+    }
+}
